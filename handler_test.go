70955174f062080e656efddc54ac834dd57436a9
caixw <xnotepad@gmail.com>
2020-02-23 22:13:15 +0800

    test: 添加 handler 的测试内容

--- handler_test.go
+++ handler_test.go
@@ -3,6 +3,9 @@
 package jsonrpc
 
 import (
+	"encoding/json"
+	"errors"
+	"math"
 	"testing"
 
 	"github.com/issue9/assert"
@@ -51,4 +54,103 @@ func TestNewHandler(t *testing.T) {
 	a.NotPanic(func() {
 		newHandler(func(bool, *int, *int) error { return nil })
 	})
+}
+
+func TestHandler_call(t *testing.T) {
+	a := assert.New(t)
+
+	data := []*struct {
+		h      *handler
+		in     string // 输入的 request.Params 数据，为 JSON 格式数据
+		out    string // call 返回的 response 实例内容，为 JSON 格式数据
+		err    int    // 错误代码，对应 Error.Code 字段，如果为 0 表示没有错误，-1 表示普通错误
+		notify bool   // 是否请求为通知类型
+	}{
+		{
+			h:   newHandler(func(bool, *int, *int) error { return nil }),
+			in:  "5",
+			out: "0",
+		},
+
+		{
+			h: newHandler(func(notify bool, in *int, out *int) error {
+				*out = (*in) + 1
+				return nil
+			}),
+			in:  "5",
+			out: "6",
+		},
+
+		{ // 处理函数返回的普通错误
+			h:   newHandler(func(bool, *int, *int) error { return errors.New("error") }),
+			in:  "5",
+			out: "0",
+			err: CodeInternalError,
+		},
+
+		{ // 处理函数返回的 *Error
+			h:   newHandler(func(bool, *int, *int) error { return NewError(CodeMethodNotFound, "not found") }),
+			in:  "5",
+			out: "0",
+			err: CodeMethodNotFound,
+		},
+
+		{ // 无效的输入 json
+			h:   newHandler(func(bool, *int, *int) error { return nil }),
+			in:  "{xx",
+			out: "0",
+			err: CodeParseError,
+		},
+
+		{ // 无效的输出 json
+			h: newHandler(func(notify bool, in *int, out *float64) error {
+				*out = math.NaN()
+				return nil
+			}),
+			in:  "5",
+			out: "0",
+			err: CodeParseError,
+		},
+
+		{ // 无效的输出 json，但是 notify 类型，无视输出，也就无法输出 json 格式错误
+			h: newHandler(func(notify bool, in *int, out *float64) error {
+				*out = math.NaN()
+				return nil
+			}),
+			in:     "5",
+			notify: true,
+			out:    "0",
+		},
+	}
+
+	for i, item := range data {
+		in := []byte(item.in)
+		req := &request{
+			Version: Version,
+			ID:      &ID{isNumber: true, number: 1},
+			Method:  "f1",
+			Params:  (*json.RawMessage)(&in),
+		}
+
+		if item.notify {
+			req.ID = nil
+		}
+
+		resp, err := item.h.call(req)
+
+		switch item.err {
+		case 0: // 正常
+			a.NotError(err, "not error at %d,%v", i, err)
+			if !item.notify {
+				a.Equal(string(*resp.Result), item.out, "not equal v1=%v,v2=%v at %d", string(*resp.Result), item.out, i)
+			}
+		case -1: // 普通错误
+			a.Error(err, "err %v at @d", err, i).Nil(resp)
+		default:
+			err1, ok := err.(*Error)
+			a.True(ok).
+				Equal(err1.Code, item.err, "not equal v1=%v,v2=%v @ %d", err1.Code, item.err, i).
+				Nil(resp)
+		}
+	}
 }
